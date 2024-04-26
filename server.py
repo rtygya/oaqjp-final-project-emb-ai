a6ba0388095a3e59ac890dfc7cf48dcd8e13bc05
rtygya <rtygya@github.com>
2024-04-26 18:24:22 -0400

    ran PyLint static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+'''Deploying web application using Flask, importing emotion_detector function'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,23 +6,20 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    '''Display webpage'''
     return render_template('index.html')
 
 @app.route('/emotionDetector')
 def emot_detector():
+    '''Displays results on webpage'''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    
     # Handle blank entry
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
-    
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(response['anger'], 
-    response['disgust'], 
-    response['fear'],
-    response['joy'],
-    response['sadness'],
-    response['dominant_emotion'])
+    return f'''For the given statement, the system response is 'anger': {response['anger']},
+'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 
+'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}.'''
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
